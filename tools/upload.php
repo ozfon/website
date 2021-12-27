09486786f7f65e4a1ba857fbab067697899c9842
Scott Gardner <scott.gardner94@gmail.com>
2021-12-27 11:54:11 -0700

    php tool to upload files to the rpi

--- tools/upload.php
+++ tools/upload.php
@@ -1,6 +1,30 @@
 <?php
 if (isset($_POST['submit'])) {
     $file = $_FILES['file'];
-    print_r($file);
+
     $fileName = $file['name'];
+    $fileTmpName = $file['tmp_name'];
+    $fileSize = $file['size'];
+    $fileError = $file['error'];
+    $fileType = $file['type'];
+
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg', 'jpeg', 'png', 'txt', 'pdf');
+
+    if (in_array($fileActualExt, $allowed)) {
+        if ($fileError === 0) {
+            if ($fileSize < 30000000) {
+                $fileNameNew = uniqid('', true).".".$fileActualExt;
+                $fileDestination = 'uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                header("Location: index.php?uploadsuccess");
+            } else {
+                echo "There was an error uploading your file";
+            }
+        } else {
+            echo "You cannot upload files of this type";
+        }
+    }
 }
